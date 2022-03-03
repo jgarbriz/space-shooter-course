f4f9bba8e37519166e98006f55cffd23159f95ec
Juan García <garciabrizjuan@gmail.com>
2022-03-03 12:40:19 +0100

    Added basic player movement and movement limits.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     private static int PLAYER_INITIAL_POS_Y = 0;
     private static int PLAYER_INITIAL_POS_Z = 0;
 
+    [SerializeField]
+    private float _speed = 3.5f;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,5 +20,25 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        CalculateMovement();
+    }
+
+    void CalculateMovement() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // Limit the player movement on the Y axis.
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 3.8f), 0);
+
+        // Limit the player movement on the X axis.
+        if (transform.position.x >= 11.3f) {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        } else if (transform.position.x <= -11.3f) {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
