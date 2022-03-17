a41deb6dd887660e9a476b0d5369bf942a463a23
Juan García <garciabrizjuan@gmail.com>
2022-03-17 18:12:55 +0100

    Added triple shot powerup basic movement and collection behavior

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,13 +7,13 @@ public class Laser : MonoBehaviour
     [SerializeField]
     private float _speed = 8.0f;
 
-    private float _yOffset = 8.0f;
+    private float _yOffScreen = 8.0f;
 
     void Update()
     {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
-        if (transform.position.y > _yOffset) {
+        if (transform.position.y > _yOffScreen) {
             Destroy(this.gameObject);
         }
     }
---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+
+    [SerializeField]
+    private float _speed = 3.0f;
+
+    private float _yOffScreen = -4.5f;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < _yOffScreen) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == PLAYER_TAG) {
+            Destroy(this.gameObject);
+        }
+    }
+}
