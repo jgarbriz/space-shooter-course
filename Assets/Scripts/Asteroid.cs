bf697190616c0ebab4b9e3d6e7c7efb67eeb1207
Juan García <garciabrizjuan@gmail.com>
2022-04-08 13:35:37 +0200

    Added spawn manager behaviour to start spawning enemies and powerups when the asteroid is destroyed.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -9,6 +9,15 @@ public class Asteroid : MonoBehaviour
     [SerializeField] private float _rotateSpeed = 19.0f;
 
     [SerializeField] private GameObject _explosionPrefab;
+    private SpawnManager _spawnManager;
+
+    void Start() 
+    {
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        if (null == _spawnManager) {
+            Debug.LogError("The spawn manager is null");
+        }
+    }
 
     void Update()
     {
@@ -19,9 +28,14 @@ public class Asteroid : MonoBehaviour
         switch (collider.tag) {
             case LASER_TAG:
                 Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+
+                _spawnManager.StartSpawning();
+
                 Destroy(collider.gameObject);
                 Destroy(this.gameObject, 0.2f);
+                
                 break;
+
             default:
                 break;
         }
