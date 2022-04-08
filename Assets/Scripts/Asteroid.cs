aa33b54856678e6e1a9122eb95a52d794e8179cf
Juan García <garciabrizjuan@gmail.com>
2022-04-08 13:17:27 +0200

    Added explosion prefab animation. Added explosion animation when the asteroid is destroyed.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    private const string LASER_TAG = "Laser";
+
     [SerializeField] private float _rotateSpeed = 19.0f;
+
+    [SerializeField] private GameObject _explosionPrefab;
 
     void Update()
     {
         transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        switch (collider.tag) {
+            case LASER_TAG:
+                Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+                Destroy(collider.gameObject);
+                Destroy(this.gameObject, 0.2f);
+                break;
+            default:
+                break;
+        }
     }
 }
